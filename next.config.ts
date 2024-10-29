2d2e30656244cc781b93ea5d86cad5b105b3d6fd
Logan Joecks <ljoecks@gmail.com>
2024-10-29 13:02:11 -0400

    get around 2mb caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  // TODO: this gets around the 2mb cache limit for fetch requests
+  // https://github.com/shadcn-ui/ui/issues/5557#issuecomment-2436149051
+  cacheHandler: require.resolve(
+    'next/dist/server/lib/incremental-cache/file-system-cache.js',
+  ),
+}
 
-export default nextConfig;
+export default nextConfig
