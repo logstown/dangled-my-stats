0796ff0ecb9a82510089c34ac6fb1ff9953173d0
Logan Joecks <ljoecks@gmail.com>
2024-11-05 14:12:15 -0500

    better song page (layout)

--- app/song/[slug]/page.tsx
+++ app/song/[slug]/page.tsx
@@ -2,20 +2,16 @@ import { filter, find, maxBy, minBy } from 'lodash'
 import { notFound } from 'next/navigation'
 import { getAllSongs, getSongFromSets } from '@/lib/phish-service'
 import { RandomThing } from '@/components/random-thing'
-import { TimelineTwoDot } from './_components/TimelineTwoDot'
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu'
 import Link from 'next/link'
 import { Song } from '@/lib/models'
-import { SongStats } from './_components/SongStats'
 import { TimesPlayed } from './_components/TimesPlayed'
 import SongTimeline from './_components/Timeline'
 import { TimelineFrequency } from './_components/TimelineFrequency'
+import { Button } from '@/components/ui/button'
+import { SetBreakdown } from './_components/SetBreakdown'
+import { SegueBreakdown } from './_components/SegueBreakdown'
+import { MostPlayedVenue } from './_components/MostPlayedVenue'
+import { MostPlayedTour } from './_components/MostPlayedTour'
 
 export default async function SongPage({ params }: { params: { slug: string } }) {
   const { data: allSongs, error } = await getAllSongs()
@@ -43,85 +39,50 @@ export default async function SongPage({ params }: { params: { slug: string } })
     x => x.exclude === '0',
   )
 
-  const sections = [
-    {
-      name: 'Timeline',
-      link: 'timeline',
-      component: (
-        <div className='flex flex-col gap-16'>
-          <TimelineFrequency setSongs={setSongs} />
-          <SongTimeline
-            setSongs={setSongs}
-            earliestDebut={earliestDebut}
-            lastShow={latestLastPlayed}
-          />
-        </div>
-      ),
-    },
-    {
-      name: 'Stats',
-      link: 'stats',
-      component: <SongStats setSongs={setSongs} />,
-    },
-    {
-      name: 'Blurbs',
-      link: 'blurbs',
-    },
-  ]
-
   return (
     <div>
-      {/* <div className='flex flex-col gap-4 border-b-2 border-primary/10 px-16 pb-0 pt-10'> */}
-      <div className='flex flex-wrap items-center justify-between gap-8 pb-12'>
-        <div>
-          <h1 className='mb-2 text-5xl font-bold tracking-tight'>
-            {song.song}
-            {song.abbr && (
-              <span className='ml-2 text-xl font-normal'>({song.abbr})</span>
-            )}
-          </h1>
-          <h3 className='ml-2 font-light'>Original Artist: {song.artist}</h3>
-        </div>
-        <div className='flex items-center gap-8'>
-          <TimesPlayed
-            timesPlayed={Number(song.times_played)}
-            mostPlayedSongCount={Number(mostPlayedSongCount)}
-          />
-          <RandomThing allSongs={allSongs} />
-        </div>
-      </div>
-      {/* <NavigationMenu className='pb-2'>
-          <NavigationMenuList className='space-x-4'>
-            {sections.map(x => (
-              <NavigationMenuItem key={x.link}>
-                <Link href={`#${x.link}`} legacyBehavior passHref>
-                  <NavigationMenuLink
-                    className={
-                      navigationMenuTriggerStyle() +
-                      ' text-xl font-bold text-primary/85'
-                    }
-                  >
-                    {x.name}
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu> */}
-      {/* </div> */}
-      {/* <div className='flex flex-col gap-24'>
-        {sections.map(x => (
-          <div key={x.link}>
-            <h1
-              id={x.link}
-              className='mb-8 text-2xl font-semibold leading-none tracking-tight'
-            >
-              {x.name}
+      <div className='flex flex-wrap items-baseline justify-between gap-8 pb-12'>
+        <div className='flex flex-wrap items-center gap-8'>
+          <div>
+            <h1 className='mb-2 text-5xl font-bold tracking-tight'>
+              {song.song}
+              {song.abbr && (
+                <span className='ml-2 text-xl font-normal'>({song.abbr})</span>
+              )}
             </h1>
-            {x.component}
+            <h3 className='ml-2 font-light'>Original Artist: {song.artist}</h3>
           </div>
-        ))}
-      </div> */}
+          <div>
+            <TimesPlayed
+              timesPlayed={Number(song.times_played)}
+              mostPlayedSongCount={Number(mostPlayedSongCount)}
+            />
+          </div>
+          <div>
+            <div className='flex items-baseline justify-end'>
+              <span className='font-bold text-zinc-400'>Debut:</span>
+              <Button asChild variant='link'>
+                <Link href={song.debut_permalink} target='_blank'>
+                  {song.debut}
+                </Link>
+              </Button>
+            </div>
+            <div className='flex items-baseline justify-end'>
+              <span className='font-bold text-zinc-400'>Last Played:</span>
+              <Button asChild variant='link'>
+                <Link href={song.last_permalink} target='_blank'>
+                  {song.last_played}
+                </Link>
+              </Button>
+            </div>
+          </div>
+          {/* <div className='flex items-baseline gap-2'>
+            <span className='font-bold text-zinc-400'>Gap:</span>
+            <span>{song.gap}</span>
+          </div> */}
+        </div>
+        <RandomThing allSongs={allSongs} />
+      </div>
       <div className='flex flex-col gap-16'>
         <TimelineFrequency setSongs={setSongs} />
         <SongTimeline
@@ -129,7 +90,22 @@ export default async function SongPage({ params }: { params: { slug: string } })
           earliestDebut={earliestDebut}
           lastShow={latestLastPlayed}
         />
-        <SongStats setSongs={setSongs} />,
+        <div className='flex flex-col gap-8 lg:flex-row'>
+          <div className='w-full lg:w-1/2'>
+            <SetBreakdown setSongs={setSongs} />
+          </div>
+          <div className='w-full lg:w-1/2'>
+            <SegueBreakdown setSongs={setSongs} />
+          </div>
+        </div>
+        <div className='flex flex-col gap-8 lg:flex-row'>
+          <div className='w-full lg:w-1/2'>
+            <MostPlayedVenue setSongs={setSongs} />
+          </div>
+          <div className='w-full lg:w-1/2'>
+            <MostPlayedTour setSongs={setSongs} />
+          </div>
+        </div>
       </div>
     </div>
   )
