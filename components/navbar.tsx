b2cb2ea4f5395718d4b71e2bcb0171c7a4bdd50a
Logan Joecks <ljoecks@gmail.com>
2024-11-21 15:24:08 -0500

    add image to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,18 +10,28 @@ import Link from 'next/link'
 import { RandomThing } from './random-thing'
 import { shirkhand } from '@/app/fonts/fonts'
 import { SearchAllServer } from './search-all-server'
+import Image from 'next/image'
 
 export async function Navbar() {
   return (
-    <header className='flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-primary px-4 text-primary-foreground shadow-xl'>
+    <header className='flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-primary px-4 text-primary-foreground shadow-2xl'>
       <div className='md:hidden'>
         <NavSheet />
       </div>
-      <div className='flex items-center'>
-        <Link href='/' className={` ${shirkhand.className} text-2xl text-secondary`}>
-          Dangled My Stats
+      <div className='flex items-center gap-20'>
+        <Link href='/' className='flex items-end gap-6'>
+          <Image
+            src='/ai-thing.png'
+            className='hidden rounded-xl md:block'
+            width={35}
+            height={35}
+            alt='thing'
+          />
+          <span className={` ${shirkhand.className} text-2xl text-secondary`}>
+            Dangled My Stats
+          </span>
         </Link>
-        <NavigationMenu className='ml-14 hidden md:block'>
+        <NavigationMenu className='hidden md:block'>
           <NavigationMenuList className='flex-row'>
             <NavigationMenuItem>
               <Link href='/browse/songs' legacyBehavior passHref>
