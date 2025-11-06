26bcf64a88f7bcb86bfc9c7ffd37d572ad13c4fd
Logan Joecks <ljoecks@gmail.com>
2025-11-06 10:45:58 -0500

    Update html-text.tsx

--- components/html-text.tsx
+++ components/html-text.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 interface HtmlTextProps {
   children: string
   className?: string
-  as?: keyof JSX.IntrinsicElements
+  as?: keyof React.JSX.IntrinsicElements
 }
 
 /**
  * Component to safely render text with HTML entities decoded.
  * Use this for displaying data from the Phish API that may contain HTML entities like &amp;
- * 
+ *
  * @example
  * <HtmlText>R&amp;R</HtmlText> // Renders as: R&R
  * <HtmlText className="text-xl" as="span">Rock &amp; Roll</HtmlText>
  */
-export function HtmlText({ 
-  children, 
-  className, 
-  as: Component = 'span' 
+export function HtmlText({
+  children,
+  className,
+  as: Component = 'span',
 }: HtmlTextProps) {
   return (
     <Component
@@ -26,4 +26,3 @@ export function HtmlText({
     />
   )
 }
-
